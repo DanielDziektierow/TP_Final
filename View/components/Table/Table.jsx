1ac99dbb34cbed20a553ad97598ed07077e3e42d
Daniel <dz09dz@hotmail.com>
2022-09-29 23:42:42 -0300

    v8.1

--- View/components/Table/Table.jsx
+++ View/components/Table/Table.jsx
@@ -2,6 +2,24 @@ import React from "react";
 
 const Table = ({listData}) => {
   
+  const eliminar = (event) => {
+    event.preventDefault()
+    const options = {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(datos)
+    };
+    
+    // Petición HTTP
+    fetch("http://localhost:3000/api", options)
+      .then(response => response.json())
+      .then(data => console.log(data)
+      );
+
+}
+  
     return(
         <main>
             <table>
@@ -19,6 +37,14 @@ const Table = ({listData}) => {
                         <td key={key}>{dt.vesselname}</td>
                         <td key={key}>{dt.depature}</td>
                         <td key={key}>{dt.arrival}</td>
+                        <td>
+                            <button  className="btn light-blue darken-4">
+                              <i className="material-icons"><img src="img/tachito.png" height='35px' /></i> 
+                            </button>
+                            <button  className="btn light-blue darken-4" style={{margin: '4px'}}>
+                              <i className="material-icons"><img src="img/lapiz.png" height='35px' /></i>
+                            </button>
+                          </td>
                       </tr>
                       )
                     })}
